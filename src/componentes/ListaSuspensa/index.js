5f34e02e3f14ce9df1c67104a90524b2e7690626
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2024-02-07 23:33:43 -0300

    iniciando lista suspensa

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,14 @@
+import "./ListaSupensa.css";
+
+const ListaSupensa = (props) => {
+    return (
+        <div>
+            <label>props.label</label>
+            <select>
+                <option></option>
+            </select>
+        </div>
+    )
+};
+
+export default ListaSupensa;
